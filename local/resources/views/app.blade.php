f7379a8d0e9ffaeea9cdfc068b01060ae71738ff
Tomkilica <tomislava.zivojinovic@gmail.com>
2016-11-01 17:59:33 +0100

    remove old js add img, html and css to home page

--- local/resources/views/app.blade.php
+++ local/resources/views/app.blade.php
@@ -1,14 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Lala</title>
+	<link rel="stylesheet" type="text/css" href="./local/resources/assets/css/main.css">
 </head>
 <body>
 	<header >
+		<img src="./local/resources/assets/img/home-panda-logo.png">
+		<ul>
+			<li>
+				<a href="#">O Nama</a>
+				<div class="grey-underline"></div>
+			</li>
+			<li><a href="#">Proizvodi</a></li>
+			<li><a href="#">Kontakt</a></li>
+		</ul>
 	</header>
+
 
 	@yield('content')
 
--- local/resources/views/home.blade.php
+++ local/resources/views/home.blade.php
@@ -2,7 +2,8 @@
 
 
 @section('content')
-	<div>
-	</div>
+	<div class="cover-img">
+			<img src="./local/resources/assets/img/cover2.jpg">
+		</div>	
 	
 @endsection
